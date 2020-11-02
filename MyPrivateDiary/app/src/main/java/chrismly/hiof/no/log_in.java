c33284e00950534084fa782666ba569b5976edb8
CrispChamping <43811094+CrispChamping@users.noreply.github.com>
2020-11-02 17:00:54 +0100

    login screen mostly done

--- MyPrivateDiary/app/src/main/java/chrismly/hiof/no/log_in.java
+++ MyPrivateDiary/app/src/main/java/chrismly/hiof/no/log_in.java
@@ -1,6 +1,5 @@
 package chrismly.hiof.no;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,25 +14,74 @@ public class log_in extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.log_in);
 
-        //Add the button behavior
-        final Button checkPassword = findViewById(R.id.checkPassword);
+        //Var
+        final Button loginBtn = findViewById(R.id.loginBtn);
+        final Button createBtn = findViewById(R.id.createBtn);
         final TextView welcomeUser = findViewById(R.id.welcomeUser);
+        final Button checkPassword = findViewById(R.id.checkPassword);
+        final Button createUser = findViewById(R.id.createUser);
+        final EditText passwordInput = findViewById(R.id.passwordInput);
+        final EditText usernameInput = findViewById(R.id.usernameInput);
+        final Button gobackBtn = findViewById(R.id.gobackBtn);
 
+        //Go back to start
+        gobackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginBtn.setVisibility(View.VISIBLE);
+                createBtn.setVisibility(View.VISIBLE);
+                welcomeUser.setVisibility(View.GONE);
+                checkPassword.setVisibility(View.GONE);
+                createUser.setVisibility(View.GONE);
+                usernameInput.setVisibility(View.GONE);
+                passwordInput.setVisibility(View.GONE);
+                gobackBtn.setVisibility(View.GONE);
+            }
+        });
+
+        //Login
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginBtn.setVisibility(View.GONE);
+                createBtn.setVisibility(View.GONE);
+                welcomeUser.setText("Welcome back");
+                welcomeUser.setVisibility(View.VISIBLE);
+                checkPassword.setVisibility(View.VISIBLE);
+                usernameInput.setVisibility(View.VISIBLE);
+                passwordInput.setVisibility(View.VISIBLE);
+                gobackBtn.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        //Check password
         checkPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final EditText passwordInput = findViewById(R.id.passwordInput);
                 final String password = passwordInput.getText().toString();
                 if(password.equals("password")) {
                     setContentView(R.layout.main_page);
                 }
-                /*else{
-                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                    builder.setTitle("Wrong password");
-                    builder.setMessage("Please try again");
-                    builder.setPositiveButton("ok", null);
-                    AlertDialog dialog = builder.show();
-                }*/
+            }
+        });
+
+        //Create new user
+        createBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginBtn.setVisibility(View.GONE);
+                createBtn.setVisibility(View.GONE);
+                welcomeUser.setText("Welcome");
+                welcomeUser.setVisibility(View.VISIBLE);
+                createUser.setVisibility(View.VISIBLE);
+                usernameInput.setVisibility(View.VISIBLE);
+                passwordInput.setVisibility(View.VISIBLE);
+                gobackBtn.setVisibility(View.VISIBLE);
+
+                //create a way to save users :)
+
+
             }
         });
 
